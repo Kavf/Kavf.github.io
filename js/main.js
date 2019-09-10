d30c00d63561704c563cf73c1d2493d0840f9c54
Kanghongbin <158989627@qq.com>
2019-09-10 22:10:12 +0800

    Site updated: 2019-09-10 22:10:12

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function(){
+	$(window).on('scroll',function(){
+		var sTop = $(window).scrollTop();
+		if(sTop > $('header').height()){
+			$('.header_wrapper').addClass('fixed');
+		}else{
+			$('.header_wrapper').removeClass('fixed');
+		}
+	});
+	$('header .bar a.tel').hover(function(){
+		$('header .bar .tel_box').addClass('active');
+	},function(){
+		$('header .bar .tel_box').removeClass('active');
+	});
+	$('header .bar a.qr').hover(function(){
+		$('header .bar .qr_box').addClass('active');
+	},function(){
+		$('header .bar .qr_box').removeClass('active');
+	});
+})();
