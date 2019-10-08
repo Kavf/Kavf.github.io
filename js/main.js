35e785d8e89f7ac82f7ec8caebf37194e293e3c1
Kanghongbin <158989627@qq.com>
2019-10-08 21:38:01 +0800

    Site updated: 2019-10-08 21:37:58

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ $(document).ready(function(){
 			$('.header_wrapper').removeClass('fixed');
 		}
 	});
+	resizeWindow();
+	$(window).on('resize',function(){
+		resizeWindow();
+	});
 	$('header .bar a.tel').hover(function(){
 		$('header .bar .tel_box').addClass('active');
 	},function(){
@@ -18,4 +22,23 @@ $(document).ready(function(){
 		$('header .bar .qr_box').removeClass('active');
 	});
 	$('.page').css('min-height', $(document).innerHeight() - $('header').height() - $('footer').height() + 'px');
+	$('header .bar a.menu').on('click',function(){
+		if(!$(this).hasClass('on')){
+			$(this).addClass('on');
+		}else{
+			$(this).removeClass('on');
+		}
+		$('.nav_bar').toggleClass('hidden-xs');
+	});
+	$(document).bind("click", function (e) {
+		if ($(e.target).closest(".nav_bar").length == 0 && $(e.target).closest("header .bar a.menu").length == 0) {
+			$(".nav_bar").addClass('hidden-xs');
+			$('header .bar a.menu').removeClass('on');
+		}
+	});
 });
+
+function resizeWindow(){
+	var bannerHeight = 0.41 * $('.swp_banner').width();
+	$('.swp_banner').css('height',bannerHeight+'px');
+}
